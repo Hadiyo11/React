bab45d6f283a85bfbf6679ba0694747e40e5ffd7
Hadiyo11 <hadyhope@gmail.com>
2023-10-05 15:32:44 -0700

    new work

---
+++ Practice/Tic-Tac-toe/tic-tac-toe/src/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const Square = (props) =>{
+  return(
+    <div className="square">
+      {props.value}
+    </div>
+  )
+}
+
+const Board = () => {
+const renderSquare = (i) =>{
+  return (
+    <Square value={i} />
+  )
+}
+
+  return (
+    <div
+      style={{
+        backgroundColor: "skyblue",
+        margin: 10,
+        padding: 20,
+      }}
+    >
+      Board
+      <div className="board-row">
+      {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
+      </div>
+      <div className="board-row">
+      {renderSquare(3)}{renderSquare(4)}{renderSquare(5)}
+      </div>
+      <div className="board-row">
+      {renderSquare(6)}{renderSquare(7)}{renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+const Game = () => {
+  return (
+    <div
+      className="game"
+    >
+      Game
+      <Board />
+    </div>
+  );
+};
+
+ReactDOM.render(<Game />, document.getElementById("root"));
