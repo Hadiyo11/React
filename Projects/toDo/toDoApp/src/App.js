bd1ef52cf0c44540d7d1f4fa31ce62a1cb0da99b
Hadiyo11 <hadyhope@gmail.com>
2023-10-09 12:35:44 -0700

    new work

---
+++ Projects/toDo/toDoApp/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import TodoApp from './toDo/todoApp'
+
+class App extends React.Component {
+
+  render(){
+
+  return (
+    <TodoApp />
+  );
+
+  }
+}
+export default App;
